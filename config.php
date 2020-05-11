2131e83575de24502513cc061267848294988e13
targhount1989 <58824254+targhount1989@users.noreply.github.com>
2020-05-11 04:38:22 +0000

    Update config.php

--- config.php
+++ config.php
@@ -12,17 +12,18 @@ $site_url = "http://love.freevar.com";
 
 /***   google adsense validation ID    ***/
 $adsense_validation = "ca-pub-XXXXXXXXXXXXXXXX";
-
-
+"ca-pub-2738192887131924"
+"9386192675"
 /***   data ad client small   ***/
 $small_ads_id = "ca-pub-XXXXXXXXXXXXXXXX";
 $small_ads_slot = "XXXXXXXXXX";
           
-
+"ca-pub-2738192887131924"
+          "9386192675"
 /***   data ad client big    ***/
 $big_ads_id = "ca-pub-XXXXXXXXXXXXXXXX";
 $big_ads_slot = "XXXXXXXXXX";
-
+"
 /***   google analytics ID    ***/
 $analytics_tracking = "UA-XXXXXXXXX-1";
 
